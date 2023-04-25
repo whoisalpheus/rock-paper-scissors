eb7e0f75bf6c7528eda8e255a3a1091d67dd51fa
whoisalpheus <104221606+whoisalpheus@users.noreply.github.com>
2023-04-25 13:52:51 -0400

    Working on playRound function

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
 
 function getComputerChoice() {
     const choice = ['rock', 'paper', 'scissors'];
-    return choice[Math.floor(Math.random() * choice.length)];
-}; 
-console.log(getComputerChoice()); // returning the index of the array instead of str???
+    console.log(choice[Math.floor(Math.random() * choice.length)]);
+} 
+// console.log(getComputerChoice()); // returning the index of the array instead of str???
+
+// Get playerSelection input
+// const input = prompt('Rock, Paper, or Scissors?');
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection == computerSelection) {
+        return 'It is a tie!';
+    } else if(
+        (playerSelection == 'rock' && computerSelection == 'scissors') || 
+        (playerSelection == 'paper' && computerSelection == 'rock') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper')) {
+            return 'You win!';
+    } else {
+            return 'You lose!';
+    }
+}
+
+const playerSelection = 'rock'; //input.toLowerCase();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+// LOSES....not needed bc we alreadt have tied and win!
+// if (
+//     (playerSelection == 'rock' && computerSelection == 'paper') || 
+//     (playerSelection == 'paper' && computerSelection == 'scissors') ||
+//     (playerSelection == 'scissors' && computerSelection == 'rock'))
