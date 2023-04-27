07eb3e40d01495f1fd5663b498eb736a6ad41d20
whoisalpheus <104221606+whoisalpheus@users.noreply.github.com>
2023-04-27 17:24:47 -0400

    Finished game()...need to test still, should be good but still getting inaccuracy

--- main.js
+++ main.js
@@ -1,31 +1,46 @@
-const choice = ['rock', 'paper', 'scissors'];
+let playerScore = 0;
+let compScore = 0;
 
 function getComputerChoice() {
-    const randomNum = Math.floor(Math.random() * 3)
-    console.log(choice[randomNum])
+    const choice = ['rock', 'paper', 'scissors'];
+    const randomNum = Math.floor(Math.random() * choice.length)
     return choice[randomNum];
-} 
-
-// Get playerSelection input
-// const input = prompt('Rock, Paper, or Scissors?');
+}
 
 function playRound(playerSelection, computerSelection) {
-    if(playerSelection === computerSelection) {
+    if (playerSelection === computerSelection) {
         return 'It is a tie!';
-    } else if(
+    } else if (
         (playerSelection === 'rock' && computerSelection === 'scissors') || 
         (playerSelection === 'paper' && computerSelection === 'rock') ||
         (playerSelection === 'scissors' && computerSelection === 'paper')) {
             return 'You win!';
-    } else if(
+            playerScore++;
+    } else if (
         (playerSelection === 'rock' && computerSelection === 'paper') || 
         (playerSelection === 'paper' && computerSelection === 'scissors') ||
         (playerSelection === 'scissors' && computerSelection === 'rock'))
         {
             return 'You lose!';
+            compScore++;
     }
 }
 
-const playerSelection = 'rock'; //input.toLowerCase();
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt('Choose', 'Rock, Paper, Scissors').toLowerCase();
+        const computerSelection = getComputerChoice();
+        console.log('1 ', playRound(playerSelection, computerSelection));
+    }
+
+    if (playerScore > compScore) {
+        return 'You beat the computer!';
+    } else if (playerScore < compScore) {
+        return 'The computer beat you, dude!';
+    } else {
+        return 'It is a tie with the computer.';
+    }
+}
+
+console.log('2 ', game());
