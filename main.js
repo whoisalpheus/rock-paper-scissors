475f66cf36a70eae573e0d530495d8eb714f0612
whoisalpheus <104221606+whoisalpheus@users.noreply.github.com>
2023-05-15 14:27:27 -0400

    Still not working, changed punctuation, maybe something to do with id/forEach function?

--- main.js
+++ main.js
@@ -6,35 +6,72 @@ const resultsDiv = document.querySelector('#results');
 
 // Computer selection of rock, paper, scissor
 function getComputerChoice() {
-    const choice = ['rock', 'paper', 'scissors'];
+    const choice = ['Rock', 'Paper', 'Scissors'];
     const randomNum = Math.floor(Math.random() * choice.length)
     return choice[randomNum];
 }
 
 // Result of each match up: rock vs paper, scissor vs rock, etc
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
+    if (playerSelection == computerSelection) {
         const p = document.createElement('p');
         p.innerText = 'It is a tie!';
         resultsDiv.appendChild(p);
 
     } else if (
-        (playerSelection === 'rock' && computerSelection === 'scissors') || 
-        (playerSelection === 'paper' && computerSelection === 'rock') ||
-        (playerSelection === 'scissors' && computerSelection === 'paper')) {
+        (playerSelection == 'Rock' && computerSelection == 'Scissors') || 
+        (playerSelection == 'Paper' && computerSelection == 'Rock') ||
+        (playerSelection == 'Scissors' && computerSelection == 'Paper')) {
             // playerScore++;
+            const p = document.createElement('p');
             p.innerText = 'You win!';
             resultsDiv.appendChild(p);
+
     } else if (
-        (playerSelection === 'rock' && computerSelection === 'paper') || 
-        (playerSelection === 'paper' && computerSelection === 'scissors') ||
-        (playerSelection === 'scissors' && computerSelection === 'rock'))
-        {
+        (playerSelection == 'Rock' && computerSelection == 'Paper') || 
+        (playerSelection == 'Paper' && computerSelection == 'Scissors') ||
+        (playerSelection == 'Scissors' && computerSelection == 'Rock'))
+    {
             // compScore++;
+            const p = document.createElement('p');
             p.innerText = 'You lose!';
             resultsDiv.appendChild(p);
     }
 }
+
+// UI
+
+// Create three buttons for Rock, Paper, Scissors
+
+// Buttons
+const rockButton = document.createElement('button');
+rockButton.textContent = 'Rock';
+rockButton.setAttribute('id', 'rock');
+container.appendChild(rockButton);
+
+const paperButton = document.createElement('button');
+paperButton.textContent = 'Paper';
+paperButton.setAttribute('id', 'paper');
+container.appendChild(paperButton);
+
+const scissorsButton = document.createElement('button');
+scissorsButton.textContent = 'Scissors';
+scissorsButton.setAttribute('id', 'scissors');
+container.appendChild(scissorsButton);
+
+// Eventlisteners
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const computerSelection = getComputerChoice();
+        const playerSelection = button.id;
+        playRound(playerSelection, computerSelection);
+    }); 
+});
+
+
+
+
 
 // Play 5 rounds
 // function game() {
@@ -55,55 +92,3 @@ function playRound(playerSelection, computerSelection) {
 //         return 'It is a tie with the computer.';
 //     }
 // } console.log(game());
-
-// UI
-
-// Create three buttons for Rock, Paper, Scissors
-
-
-// Add a div for displaying results
-// const results = document.createElement('div');
-// results.setAttribute('id', 'results');
-// container.appendChild(results);
-
-// Buttons
-const rockButton = document.createElement('button');
-rockButton.textContent = 'Rock';
-rockButton.setAttribute('id', 'rockBtn');
-// rockButton.classList.add('btn');
-container.appendChild(rockButton);
-
-const paperButton = document.createElement('button');
-paperButton.textContent = 'Paper';
-paperButton.setAttribute('id', 'paperBtn');
-// paperButton.classList.add('btn');
-container.appendChild(paperButton);
-
-const scissorsButton = document.createElement('button');
-scissorsButton.textContent = 'Scissors';
-scissorsButton.setAttribute('id', 'scissorsBtn');
-// scissorsButton.classList.add('btn');
-container.appendChild(scissorsButton);
-
-// let computerSelection = getComputerChoice();
-// let playerSelection = button.id;
-
-// Eventlisteners
-
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        const computerSelection = getComputerChoice();
-        const playerSelection = button.id;
-        playRound(playerSelection, computerSelection);
-    });
-});
-
-// btn.forEach((button) => {
-//     button.addEventListener('click', playRound(playerSelection, computerSelection) => {
-//         let playerSelection = button.id;
-//         // let computerSelection = getComputerChoice();
-//         // console.log(playRound(playerSelection, computerSelection));
-//     });
-// });
-
-// console.log(btn.forEach(button => button.addEventListener('click', playRound)));
